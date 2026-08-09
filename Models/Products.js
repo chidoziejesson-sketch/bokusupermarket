13562799921d60bcb7e28c4e1e40bafe38ea1cdf
chidoziejesson-sketch <chidoziejesson@gmail.com>
2026-08-09 18:40:33 +0100

    firstpush

---
+++ Models/Products.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    timestamps: true //Date created and updated at
+});
+// create model from schema
+const Product = mongoose.model('Product', productSchema);
---
+++ Models/Users.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    
+        timestamps: true //Date created and updated at
+    
+});
+
+// create model from schema
+const User = mongoose.model('User', userSchema);
