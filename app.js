6dbd5d5654caa88db3b728b3d363736f7af34834
chidoziejesson-sketch <chidoziejesson@gmail.com>
2026-08-09 22:14:11 +0100

    git commit -m "Added product model and routes, updated app.js to include product routes, added dotenv for environment variables, and updated package.json scripts."

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-require('dotenv').config();
 const express = require('express');
-const productRoutes = require('./Routes/ProductRoute');
+const dotenv = require('dotenv');
+const connectDB = require('./Config/databaseConfig');
 const app = express();
+const productRoutes = require('./Routes/ProductRoute');
+
+// Load environment variables from .env file
+dotenv.config();
+connectDB(); // Connect to MongoDB
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 
-app.use('/products', productRoutes);
+app.use('/products', productRoutes); // Use product routes for /products endpoint
 
 const port = process.env.PORT || 3300;
 app.listen(port, () => {
