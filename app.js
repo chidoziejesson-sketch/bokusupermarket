fd274ff7968679492a70181859da7df2908866bf
chidoziejesson-sketch <chidoziejesson@gmail.com>
2026-08-09 21:58:08 +0100

    git commit -m "Added start and dev scripts to package.json, added dotenv dependency, updated product model with timestamps, and set up basic express app with product routes."

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const productRoutes = require('./Routes/ProductRoute');
 const app = express();
@@ -6,6 +7,7 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.use('/products', productRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const port = process.env.PORT || 3300;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
