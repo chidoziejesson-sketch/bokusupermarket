e339fb44d05f88239a1b87bdd76b34e0da04246c
chidoziejesson-sketch <chidoziejesson@gmail.com>
2026-08-19 10:57:52 +0100

    git

---
+++ Middleware/upload.js
@@ -0,0 +1,13 @@
+// Upload images to Cloudinary
+const multer = require('multer');
+const {CloudinaryStorage} = require('multer-storage-cloudinary');
+const cloudinary = require('../Config/cloudinary');
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'bokusupermarket', // Folder name in Cloudinary
+        allowed_formats: ['jpg', 'jpeg', 'png'], // Allowed image formats
+        transformation: [{ width: 500, height: 500, crop: 'limit' }], // Resize images to a maximum of 500x500 pixels
+    },
+});
